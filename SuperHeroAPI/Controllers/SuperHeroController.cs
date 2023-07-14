8798c66842455b9347baa39e8ce7afcc486a1c1e
DonaldMod <bmodiba87@gmail.com>
2023-07-14 08:43:40 +0200

    Initial commit

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public SuperHeroController(DataContext _context)
+        {
+            context = _context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return Ok(await context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> CreateSuperHero(SuperHero hero)
+        {
+            context.SuperHeroes.Add(hero);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHero(SuperHero hero)
+        {
+            var dbhero = await context.SuperHeroes.FindAsync(hero.Id);
+            if (dbhero == null)
+            {
+                return BadRequest("Hero not found");
+            }
+
+            dbhero.Name = hero.Name;
+            dbhero.FirstName = hero.FirstName;
+            dbhero.LastName = hero.LastName;
+            dbhero.Place = hero.Place;
+
+            await context.SaveChangesAsync();
+
+            return Ok(await context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperhero(int id)
+        {
+            var dbhero = await context.SuperHeroes.FindAsync(id);
+            if (dbhero == null)
+            {
+                return BadRequest("Hero not found");
+            }
+
+            context.SuperHeroes.Remove(dbhero);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.SuperHeroes.ToListAsync());
+        }
+    }
+}
